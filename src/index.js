de82df068e2099bdd4729135fe7d1bbd301798ba
Jasmeet Singh <jasmeetsinghbhatia@gmail.com>
2021-01-01 17:50:44 -0500

    books as separate objects using props

--- src/index.js
+++ src/index.js
@@ -4,48 +4,52 @@ import ReactDom from "react-dom";
 //css
 import "./index.css";
 
+const firstBook = {
+  title: "book1",
+  author: "author1",
+  image:
+    "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
+};
+const secondBook = {
+  title: "book2",
+  author: "author2",
+  image:
+    "https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200_SR200,200_.jpg",
+};
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        title={firstBook.title}
+        author={firstBook.author}
+        image={firstBook.image}
+      >
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate
+          consequatur itaque dignissimos rerum, eaque labore. Soluta nostrum
+          laborum facilis blanditiis.
+        </p>
+      </Book>
+      <Book
+        title={secondBook.title}
+        author={secondBook.author}
+        image={secondBook.image}
+      />
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
+  // destructure the props before using each value inside props
+  const { image, title, author, children } = props;
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={image} alt="this is book cover" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      {children}
     </article>
-  );
-};
-
-const Title = () => {
-  return <h1>I Love you to the Moon and Back</h1>;
-};
-
-const Author = () => {
-  return (
-    <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
-      Amelia Hepworth
-    </h4>
-  );
-};
-
-const Image = () => {
-  return (
-    <img
-      src="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg"
-      alt="this is book cover"
-    ></img>
   );
 };
 
