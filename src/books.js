0c142dc59c48b3e10afd50ac1601e785afa251cf
Jasmeet Singh <jasmeetsinghbhatia@gmail.com>
2021-01-01 19:07:34 -0500

    Created separate components for book and book data

---
+++ src/books.js
@@ -0,0 +1,16 @@
+export const Books = [
+  {
+    id: 1,
+    title: "book1",
+    author: "author1",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
+  },
+  {
+    id: 2,
+    title: "book2",
+    author: "author2",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200_SR200,200_.jpg",
+  },
+];
--- src/index.js
+++ src/index.js
@@ -3,54 +3,21 @@ import ReactDom from "react-dom";
 
 //css
 import "./index.css";
+// book component from book.js
+import {Book} from "./book";
+// data about books from books.js
+import { Books } from "./books";
 
-const firstBook = {
-  title: "book1",
-  author: "author1",
-  image:
-    "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
-};
-const secondBook = {
-  title: "book2",
-  author: "author2",
-  image:
-    "https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200_SR200,200_.jpg",
-};
+const booksList = Books.map((book) => {
+  return <Book book={book}/>;
+});
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        title={firstBook.title}
-        author={firstBook.author}
-        image={firstBook.image}
-      >
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate
-          consequatur itaque dignissimos rerum, eaque labore. Soluta nostrum
-          laborum facilis blanditiis.
-        </p>
-      </Book>
-      <Book
-        title={secondBook.title}
-        author={secondBook.author}
-        image={secondBook.image}
-      />
-    </section>
+      {booksList}
+      </section>
   );
 }
-
-const Book = (props) => {
-  // destructure the props before using each value inside props
-  const { image, title, author, children } = props;
-  return (
-    <article className="book">
-      <img src={image} alt="this is book cover" />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
-      {children}
-    </article>
-  );
-};
 
 ReactDom.render(<BookList />, document.getElementById("root"));
